974839e73c6a120f7b9e34701922031fa7e416d8
mohanramasamy2005-eng <mohanramasamy2005@gmail.com>
2026-03-24 16:02:58 +0530

    added a java file

---
+++ indes.java
@@ -0,0 +1,11 @@
+public class Index {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        
+        int number = 42;
+        String message = "Welcome to Java";
+        
+        System.out.println(message);
+        System.out.println("Number: " + number);
+    }
+}   
